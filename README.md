36dbe0cc0d1d81575026f6956e220e128966724c
Richard <Richard>
2020-07-27 16:39:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# pinephone-cairo-game-starter
+
+This repo is a starter for creating a cairo based game in Rust for PinePhone
+
+## Compilation
+
+Check out commands in my `Makefile` or simply run `make`.
+
+Notice how I compile glade and images into a single `.gresource` file, this let's me embed 
+the bytes of all the things my app needs into my Rust app.
+
+Currently I compile this on my desktop for quick iteration and my phone to test out.
+
+* in debug mode, the app will be about the size of a pinephone
+* in release mode, the app will maximize to take up available space (assumed to be running on phone)
+
+When i compile on the phone, I have to increase my amount of ram by uzing zram
+
+```
+sudo swapoff /dev/zram0 
+sudo zramctl --reset /dev/zram0 
+sudo zramctl --find --size 2048M
+sudo mkswap /dev/zram0 
+sudo swapon /dev/zram0
+```
+
+## Setup Rust for cross compilation
+
+I haven't figured this stuff out yet, I could use some help on these details.
+
+1. get gcc setup for aarch64
+
+```
+sudo apt-get install gcc-aarch64-linux-gnu
+```
+```
+sudo dnf install gcc-aarch64-linux-gnu
+```
+
+2. get rust setup for aarch64
+
+```
+rustup install stable-aarch64-unknown-linux-gnu
+```
+
+add to `~/.cargo/config`
+
+```
+[target.aarch64-unknown-linux-gnu]
+linker = "aarch64-linux-gnu-gcc"
+```
+
+3. Make sure you have aarch64 dependencies for Gtk
+
+???
+
+4. Start the compile!
+
+```
+PKG_CONFIG_ALLOW_CROSS=1 cargo build --target aarch64-unknown-linux-gnu --release
+```
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use gtk::prelude::*;
+use gtk::*;
+use rand::Rng;
+use std::cell::RefCell;
+use std::f64::consts::PI;
+use std::rc::Rc;
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    // we embed our image,glade file, and css  in a glib gresource file generated
+    //  from app.xml, let's load it in from bytes embedded in our app
+    let bytes = glib::Bytes::from_static(include_bytes!("app.gresource"));
+    let res = gio::Resource::from_data(&bytes).unwrap();
+    gio::resources_register(&res);
+
+    // lets generate all our controls from glade file
+    let builder = gtk::Builder::from_resource("/app/app.glade");
+
+    // grab the controls we'll be using
+    let window: gtk::Window = builder.get_object("window1").unwrap();
+    let canvas: Rc<RefCell<gtk::DrawingArea>> =
+        Rc::new(RefCell::new(builder.get_object("canvas").unwrap()));
+
+    // handle draw and use cairo context
+    canvas.borrow_mut().connect_draw(|_, ctx| {
+        let mut rng = rand::thread_rng();
+        ctx.scale(100f64, 100f64);
+        let rgb = (rng.gen(), rng.gen(), rng.gen());
+        ctx.set_source_rgba(rgb.0, rgb.1, rgb.2, 0.6);
+        ctx.arc(0.40, 0.53, 0.2, 0.0, PI * 2.);
+        ctx.fill();
+        let rgb = (rng.gen(), rng.gen(), rng.gen());
+        ctx.set_source_rgba(rgb.0, rgb.1, rgb.2, 0.6);
+        ctx.arc(0.5, 0.65, 0.2, 0.0, PI * 2.);
+        ctx.fill();
+        let rgb = (rng.gen(), rng.gen(), rng.gen());
+        ctx.set_source_rgba(rgb.0, rgb.1, rgb.2, 0.6);
+        ctx.arc(0.6, 0.53, 0.2, 0.0, PI * 2.);
+        ctx.fill();
+
+        Inhibit(false)
+    });
+
+    // show the window
+    window.show_all();
+
+    // don't maximize in debug (we assume debug is desktop)
+    // on phone we should maximize
+    #[cfg(not(debug_assertions))]
+    window.maximize();
+
+    gtk::main();
+}
